a89d5eceda4b1eeec6e2296e9cbb013020470f00
danishhafeez-cc <157350749+danishhafeez-cc@users.noreply.github.com>
2024-06-12 18:50:07 +0500

    Update featured_projects.dart

--- lib/ui/home/components/featured_projects.dart
+++ lib/ui/home/components/featured_projects.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio_danish/utils/app_extentions.dart';
-import 'package:portfolio_danish/utils/app_theme.dart';
+
+import '../../../utils/app_theme.dart';
 
 class FeatureProjects extends StatelessWidget {
   const FeatureProjects({super.key});
@@ -8,95 +9,43 @@ class FeatureProjects extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
-    return Padding(
+    return Container(
+      height: context.height * 0.7,
       padding: EdgeInsets.symmetric(horizontal: context.width * 0.05),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Text(
-            "Featured Projects",
-            style: textTheme.displaySmall!.copyWith(
-              color: PortfolioAppTheme.nameColor,
-            ),
+      child: GridView.builder(
+        itemCount: 3,
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          crossAxisSpacing: 8,
+          mainAxisSpacing: 10,
+        ),
+        itemBuilder: (context, __) => Container(
+          padding: const EdgeInsets.all(15),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.white,
           ),
-          SizedBox(height: context.height * 0.05),
-          // GridView.builder(
-          //     itemCount: 2,
-          //     physics: const NeverScrollableScrollPhysics(),
-          //     gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          //       crossAxisCount: 2,
-          //       crossAxisSpacing: 8,
-          //       mainAxisSpacing: 10,
-          //     ),
-          //     itemBuilder: (context, __) => Container(
-          //           width: context.width * 0.1,
-          //           height: context.height * 0.1,
-          //           color: Colors.grey,
-          //         ))
-          //
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: Column(
             children: [
-              Container(
-                width: context.width * 0.32,
-                height: context.height * 0.42,
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.white,
-                ),
-                child: Column(
-                  children: [
-                    Image.asset(
-                      width: context.width * 0.3,
-                      height: context.height * 0.3,
-                      "assets/imgs/fitness_challenge.png",
-                      fit: BoxFit.contain,
-                    ),
-                    SizedBox(height: context.height * 0.005),
-                    Text("Fitness Challenge App", style: textTheme.titleSmall),
-                    SizedBox(height: context.height * 0.005),
-                    ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            foregroundColor: PortfolioAppTheme.nameColor),
-                        onPressed: () {},
-                        child: const Text("Github Link"))
-                  ],
-                ),
+              Image.asset(
+                width: context.width * 0.3,
+                "assets/imgs/fitness_challenge.png",
+                fit: BoxFit.cover,
               ),
-              SizedBox(width: context.width * 0.01),
-              Container(
-                width: context.width * 0.32,
-                height: context.height * 0.42,
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.white,
-                ),
-                child: Column(
-                  children: [
-                    Image.asset(
-                      width: context.width * 0.3,
-                      height: context.height * 0.3,
-                      "assets/imgs/snaptik_video_downloader.jpg",
-                      fit: BoxFit.contain,
-                    ),
-                    SizedBox(height: context.height * 0.005),
-                    Text("Snaptik Video Downloader",
-                        style: textTheme.titleSmall),
-                    SizedBox(height: context.height * 0.005),
-                    ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            foregroundColor: PortfolioAppTheme.nameColor),
-                        onPressed: () {},
-                        child: const Text("Github Link"))
-                  ],
-                ),
-              ),
+              SizedBox(height: context.height * 0.005),
+              Text("Fitness Challenge App", style: textTheme.titleSmall),
+              SizedBox(height: context.height * 0.005),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    foregroundColor: PortfolioAppTheme.nameColor),
+                onPressed: () {},
+                child: const Text("Github Link"),
+              )
             ],
           ),
-        ],
+        ),
       ),
     );
   }
